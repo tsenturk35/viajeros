cfca0f6a90230c36ce0af1328f522d3954b9f2fe
battty08@gmail.com <battty08@gmail.com@734dc67e-35e1-3d9d-bbae-fef5016c4cc4>
2012-12-09 14:03:21 +0000

    changed AbstractDaoImpl. added generic methods

--- trunk/POC/src/main/java/com/viajeros/dao/AbstractDaoImpl.java
+++ trunk/POC/src/main/java/com/viajeros/dao/AbstractDaoImpl.java
@@ -1,16 +1,120 @@
 package com.viajeros.dao;
 
-
-
+import java.util.List;
+import java.util.Set;
+import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.classic.Session;
 
-public abstract class AbstractDaoImpl {
+public class AbstractDaoImpl {
 
-	public abstract SessionFactory getSessionFactory();
-	
-	protected Session getSession() {
+	private final SessionFactory sessionFactory;
+
+	public AbstractDaoImpl(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public Session getSession() {
 		return getSessionFactory().openSession();
 	}
-       
+
+	public Object updateEntity(Object entity) {
+		Session session = getSession();
+		Transaction transaction = session.beginTransaction();
+		transaction.begin();
+		try {
+			session.saveOrUpdate(entity);
+			transaction.commit();
+			session.flush();
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			transaction.rollback();
+			throw e;
+		} finally {
+			session.close();
+		}
+		return entity;
+	}
+
+	public List<?> updateEntities(List<?> entities) {
+		if (entities.size() == 0)
+			return entities;
+		Session session = getSession();
+		Transaction transaction = session.beginTransaction();
+		transaction.begin();
+		try {
+			for (Object entity : entities)
+				session.saveOrUpdate(entity);
+			transaction.commit();
+			session.flush();
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			transaction.rollback();
+			throw e;
+		} finally {
+			session.close();
+		}
+		return entities;
+	}
+
+	public void updateEntities(Set<?> entities) {
+		if (entities.size() == 0)
+			return;
+		Session session = getSession();
+		Transaction transaction = session.beginTransaction();
+		transaction.begin();
+		try {
+			for (Object entity : entities)
+				session.saveOrUpdate(entity);
+			transaction.commit();
+			session.flush();
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			transaction.rollback();
+			throw e;
+		} finally {
+			session.close();
+		}
+	}
+
+	public void deleteEntity(Object entity) {
+		Session session = getSession();
+		Transaction transaction = session.beginTransaction();
+		transaction.begin();
+		try {
+			session.delete(entity);
+			transaction.commit();
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			transaction.rollback();
+			throw e;
+		} finally {
+			session.close();
+		}
+	}
+
+	public void deleteEntities(List<?> entities) {
+		if (entities.size() < 1)
+			return;
+		Session session = getSession();
+		Transaction transaction = session.beginTransaction();
+		transaction.begin();
+		try {
+			for (Object entity : entities)
+				session.delete(entity);
+			transaction.commit();
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			transaction.rollback();
+			throw e;
+		} finally {
+			session.close();
+		}
+	}
+
 }
